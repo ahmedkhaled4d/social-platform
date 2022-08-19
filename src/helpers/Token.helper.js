3b9347ddcf2bb140bb584904c204ae61debf44eb
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2022-08-19 10:30:41 +0200

    improve helpers

--- src/helpers/Token.helper.js
+++ src/helpers/Token.helper.js
@@ -1,12 +1,14 @@
 /* eslint-disable require-jsdoc */
-import jwt from 'jsonwebtoken';
-import dotenv from 'dotenv';
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 class Tokenizer {
   static async generateToken(payload) {
-    const token = await jwt.sign(payload, process.env.SECRET_KEY, { expiresIn: '10h' });
+    const token = await jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "10h",
+    });
 
     return token;
   }
