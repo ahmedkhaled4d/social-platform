1efd6f9dcd4e9bca645d4959f260ef0d2a41c3b9
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2022-12-28 00:27:46 +0200

    tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,25 @@
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../src/index';
+import chai, { expect } from "chai";
+import chaiHttp from "chai-http";
+import app from "../src/index";
 
 chai.use(chaiHttp);
-describe('Unavailable URL', () => {
-  it('Should return 200 when youbtry to access root url', (done) => {
+describe("Unavailable URL", () => {
+  it("Should return 200 when youbtry to access root url", (done) => {
     chai
       .request(app)
-      .get('/')
+      .get("/")
       .end((err, res) => {
         expect(res.status).to.eql(200);
         done();
       });
   });
-  it('Should return an error message, Once you tried to access the unavailable URL', (done) => {
+  it("Should return an error message, Once you tried to access the unavailable URL", (done) => {
     chai
       .request(app)
-      .get('/fadfasfasfafafdafafda')
+      .get("/fadfasfasfafafdafafda")
       .end((err, res) => {
-        expect(res.body).to.be.an('object');
-        expect(res.body.error.message).to.deep.equal('Page Not found');
+        expect(res.body).to.be.an("object");
+        expect(res.body.error.message).to.deep.equal("Page Not found");
         done();
       });
   });
